7556ff5c3875ecbc3d8e1a861be305991d30e7d1
sara ibnabbad <saraibnabbad96@gmail.com>
2023-01-13 23:44:48 +0100

    update signup

--- resources/views/admin/signUp.blade.php
+++ resources/views/admin/signUp.blade.php
@@ -1,120 +1,132 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/form.css') }}">
     <title>Document</title>
 </head>
+
 <body>
     <section class="h-100 ">
         <div class="container py-5 h-100">
-          <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col">
-              <div class="card card-registration my-4">
-                <div class="row g-0">
-                  <div class="col-xl-6 d-none d-xl-block">
-                    <img src="{{ asset('img/signup/dd.jpg') }}"
-                      alt="doctor photo" class="img-fluid"
-                      style="border-top-left-radius: .25rem; border-bottom-left-radius: .25rem; height:132vh" />
-                  </div>
-                  <div class="col-xl-6">
-                    <div class="card-body p-md-5 text-black">
-                      <h3 class="mb-3 text-uppercase">S'enregistrer</h3>
-                      <h5 class="text-black-50">Entrez vos informations pour créer un compte</h5>
-                      
-                      <div class="form-outline mb-4">
-                        <input type="text" id="name" class="form-control form-control-lg" name="name" />
-                        <label class="form-label" for="name">Nom complet <span class="text-danger">*</span></label>
-                      </div>
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col">
+                    <div class="card card-registration my-4">
+                        <div class="row g-0">
+                            <div class="col-xl-6 d-none d-xl-block">
+                                <img src="{{ asset('img/signup/dd.jpg') }}" alt="doctor photo" class="img-fluid"
+                                    style="border-top-left-radius: .25rem; border-bottom-left-radius: .25rem; height:132vh" />
+                            </div>
+                            <div class="col-xl-6">
+                                <div class="card-body p-md-5 text-black">
+                                    <h3 class="mb-3 text-uppercase">S'enregistrer</h3>
+                                    <h5 class="text-black-50">Entrez vos informations pour créer un compte</h5>
+                                    <form action="" method="post">
+                                        @csrf
+                                        <div class="form-outline mb-4">
+                                            <input type="text" id="name" class="form-control form-control-lg"
+                                                name="name" />
+                                            <label class="form-label" for="name">Nom complet <span
+                                                    class="text-danger">*</span></label>
+                                        </div>
+                                        <div class="form-outline mb-4">
+                                            <input type="email" id="email" class="form-control form-control-lg"
+                                                name="email" />
+                                            <label class="form-label" for="email">E-mail <span
+                                                    class="text-danger">*</span></label>
+                                        </div>
 
-                      
-                        <div class="form-outline mb-4">
-                          <input type="email" id="email" class="form-control form-control-lg" name="email" />
-                          <label class="form-label" for="email">E-mail <span class="text-danger">*</span></label>
-                        </div>
-                      
 
-                      <div class="row">
-                        
-                        <div class="col-md-6 mb-4">
-                          <div class="form-outline">
-                            <input type="password" id="password" class="form-control form-control-lg" name="password" />
-                            <label class="form-label" for="password">Mot de passe <span class="text-danger">*</span></label>
-                          </div>
-                        </div>
-                        <div class="col-md-6 mb-4">
-                          <div class="form-outline">
-                            <input type="password" id="confirmation_password" class="form-control form-control-lg" name="password_confirmation"/>
-                            <label class="form-label" for="confirmation_password">Confirmer mot de passe <span class="text-danger">*</span></label>
-                          </div>
-                        </div>
-                      </div>
-      
-                      
-      
-                      <div class="form-outline mb-4">
-                        <input type="text" id="adresse" class="form-control form-control-lg" name="addres" />
-                        <label class="form-label" for="adresse">Adresse</label>
-                      </div>
-      
-                      <div class="d-md-flex justify-content-start align-items-center mb-4 py-2">
-      
-                        <h6 class="mb-0 me-4">Sexe : <span class="text-danger">*</span></h6>
-      
-                        <div class="form-check form-check-inline mb-0 me-4">
-                          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="femme"
-                            value="femme" />
-                          <label class="form-check-label" for="femme">Femme</label>
-                        </div>
-                        <div class="form-check form-check-inline mb-0 me-4">
-                          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="homme"
-                            value="homme" />
-                          <label class="form-check-label" for="homme">Homme</label>
-                        </div>
-                        
-                        
-                      </div>
+                                        <div class="row">
 
-                      
+                                            <div class="col-md-6 mb-4">
+                                                <div class="form-outline">
+                                                    <input type="password" id="password"
+                                                        class="form-control form-control-lg" name="password" />
+                                                    <label class="form-label" for="password">Mot de passe <span
+                                                            class="text-danger">*</span></label>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 mb-4">
+                                                <div class="form-outline">
+                                                    <input type="password" id="confirmation_password"
+                                                        class="form-control form-control-lg"
+                                                        name="password_confirmation" />
+                                                    <label class="form-label" for="confirmation_password">Confirmer mot
+                                                        de passe <span class="text-danger">*</span></label>
+                                                </div>
+                                            </div>
+                                        </div>
 
-                      <div class="form-outline mb-4">
-                        <input type="date" id="date" class="form-control form-control-lg" placeholder="" name="born"/>
-                        <label class="form-label" for="date">Date de naisance</label>
-                      </div>
 
-                      <div class="form-outline mb-4">
-                        <input type="text" id="telephone" class="form-control form-control-lg" name="phone"/>
-                        <label class="form-label" for="telephone">Téléphone</label>
-                      </div>
 
-                      <div class="d-md-flex justify-content-start align-items-center mb-4 py-2">
-      
-                        <h6 class="mb-0 me-4">Mutuelle : <span class="text-danger">*</span></h6>
-      
-                        <div class="form-check form-check-inline mb-0 me-4">
-                          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="oui"
-                            value="oui" />
-                          <label class="form-check-label" for="oui">Oui</label>
-                        </div>
-                        <div class="form-check form-check-inline mb-0 me-4">
-                          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="non"
-                            value="non" />
-                          <label class="form-check-label" for="non">Non</label>
-                        </div>
-                        
-                        
-                      </div>
+                                        <div class="form-outline mb-4">
+                                            <input type="text" id="adresse" class="form-control form-control-lg"
+                                                name="addres" />
+                                            <label class="form-label" for="adresse">Adresse</label>
+                                        </div>
 
-                      
-                      <div class="form-outline mb-4">
-                        <label class="form-labelv" for="picture">Photo :</label>
-                        <input type="file" id="picture" class="form-control form-control-lg" name="picture" accept="image/*"/>
-                      </div> 
-                      
-                      {{-- <div class="row">
+                                        <div class="d-md-flex justify-content-start align-items-center mb-4 py-2">
+
+                                            <h6 class="mb-0 me-4">Sexe : <span class="text-danger">*</span></h6>
+
+                                            <div class="form-check form-check-inline mb-0 me-4">
+                                                <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                                    id="femme" value="femme" />
+                                                <label class="form-check-label" for="femme">Femme</label>
+                                            </div>
+                                            <div class="form-check form-check-inline mb-0 me-4">
+                                                <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                                    id="homme" value="homme" />
+                                                <label class="form-check-label" for="homme">Homme</label>
+                                            </div>
+                                        </div>
+
+
+
+                                        <div class="form-outline mb-4">
+                                            <input type="date" id="date" class="form-control form-control-lg"
+                                                 name="born" />
+                                            <label class="form-label" for="date">Date de naisance</label>
+                                        </div>
+
+                                        <div class="form-outline mb-4">
+                                            <input type="text" id="telephone" class="form-control form-control-lg"
+                                                name="phone" />
+                                            <label class="form-label" for="telephone">Téléphone</label>
+                                        </div>
+
+                                        <div class="d-md-flex justify-content-start align-items-center mb-4 py-2">
+
+                                            <h6 class="mb-0 me-4">Mutuelle : <span class="text-danger">*</span></h6>
+
+                                            <div class="form-check form-check-inline mb-0 me-4">
+                                                <input class="form-check-input" type="radio"
+                                                    name="inlineRadioOptions" id="oui" value="oui" />
+                                                <label class="form-check-label" for="oui">Oui</label>
+                                            </div>
+                                            <div class="form-check form-check-inline mb-0 me-4">
+                                                <input class="form-check-input" type="radio"
+                                                    name="inlineRadioOptions" id="non" value="non" />
+                                                <label class="form-check-label" for="non">Non</label>
+                                            </div>
+
+
+                                        </div>
+
+
+                                        <div class="form-outline mb-4">
+                                            <h6 class="form-labelv" for="picture">Photo :</h6>
+                                            <input type="file" id="picture" class="form-control form-control-lg"
+                                                name="picture" accept="image/*" />
+                                        </div>
+
+                                        {{-- <div class="row">
                          <div class="col-md-6 mb-4">
       
                           <select class="select">
@@ -126,21 +138,25 @@
       
                         </div> 
                       </div> --}}
-      
-                      
-      
-                      <div class="d-flex justify-content-end pt-3">
-                        <button type="button" class="btn btn-success btn-lg ms-2">S'enregister</button>
-                      </div>
-      
+
+
+
+                                        <div class="d-flex justify-content-end pt-3">
+                                            <button type="button"
+                                                class="btn btn-success btn-lg ms-2">S'enregister</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </section>
+    </section>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+</script>
+
 </html>
