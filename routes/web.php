44d29ddb26016342c9be248623169386e1a2f665
FENNIR ADNANE <adnane.fennir@gmail.com>
2023-01-16 18:32:58 +0100

    PriseRdv

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,5 @@ Route::get('/auth', [AuthentController::class, 'displaySignup'])->name('signup')
 Route::get('/doc', [AuthentController::class, 'displayLog'])->name('docSignup');
 Route::get('/log', [AuthentController::class, 'displayLogin'])->name('login');
 Route::get('/staff', [AuthentController::class, 'displaystaff'])->name('staffSignup');
+Route::get('/addrdv', [AuthentController::class, 'displatAddrdv'])->name('addRdv');
 Route::get('/addptnt', [AuthentController::class, 'displatAddpatient'])->name('addPatient');
