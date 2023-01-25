e7c947c4fdb9875f3f61c63095cf6d4c3e74983c
sara ibnabbad <saraibnabbad96@gmail.com>
2023-01-25 23:03:44 +0100

    PW

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Auth;
   
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request; 
-use DB; 
+
 use Carbon\Carbon; 
 use App\Models\User; 
-use Mail; 
-use Hash;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
   
 class ForgotPasswordController extends Controller
